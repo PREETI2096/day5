5dc089bdc18928789f2e2ba850f4882a8e055b09
PREETI2096 <preetirg96@gmail.com>
2022-07-11 22:28:44 +0530

    added:singledigit file

---
+++ singledigit.sh
@@ -0,0 +1,20 @@
+read -p "enter the number:" single_digit
+case $single_digit in
+1)echo "ONE"
+;;
+2)echo "TWo"
+;;
+3)echo "THREE"
+;;
+4)echo "FOUR"
+;;
+5)echo "FIVE"
+;;
+6)echo "SIX"
+;;
+7)echo "SEVEN"
+;;
+8)echo "EIGHT"
+;;
+*)echo "NINE"
+esac
