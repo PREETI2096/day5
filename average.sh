b827a96572e9993e938db9ea6bd9f4063b1eb4c3
PREETI2096 <preetirg96@gmail.com>
2022-07-11 19:05:34 +0530

    added:average file

---
+++ average.sh
@@ -0,0 +1,16 @@
+n1=$((10+RANDOM%90))
+echo $n1
+n2=$((10+RANDOM%90))
+echo $n2
+n3=$((10+RANDOM%90))
+echo $n3
+n4=$((10+RANDOM%90))
+echo $n4
+n5=$((10+RANDOM%90))
+echo $n5
+sum=$((n1+n2+n3+n4+n5))
+echo $sum
+Average=$((sum/5))
+echo $Average
+
+
